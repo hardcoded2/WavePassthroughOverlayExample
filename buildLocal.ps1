f1f4696097b0e7fb3b00fc06d6d240e1e3bd306e
Alex Sink <alexsink@yahoo.com>
2022-03-14 16:56:29 -0700

    remove unused manual build scripts

--- buildLocal.ps1
+++
@@ -1,6 +0,0 @@
-#requires -Modules UnitySetup
-
-for (($i = 0); $i -lt 10; $i++)
-{
-    Start-UnityEditor -ExecuteMethod TestBuilder.Build -BatchMode -Quit -LogFile .\build$(get-date -f yyyy-MM-dd-hh-ss).log -Wait
-}
